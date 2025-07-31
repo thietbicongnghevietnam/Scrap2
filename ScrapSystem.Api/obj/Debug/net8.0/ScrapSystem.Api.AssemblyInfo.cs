82de8d40fd56e87ef8e2a2ca38dd66ac71068b28
Minh <thietbicongnghevietnam@gmail.com>
2025-07-31 17:21:31 +0700

    31072025ba

--- ScrapSystem.Api/obj/Debug/net8.0/ScrapSystem.Api.AssemblyInfo.cs
+++ ScrapSystem.Api/obj/Debug/net8.0/ScrapSystem.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853bb57199aa7167a2647edf868f4ed5dba8bf51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ScrapSystem.Web/System/Web/UI.cs
@@ -0,0 +1,43 @@
+﻿namespace System.Web
+{
+    public class UI
+    {
+        public class HtmlControls
+        {
+            public class HtmlForm
+            {
+            }
+
+            public class HtmlInputGenericControl
+            {
+            }
+
+            public class HtmlButton
+            {
+            }
+        }
+
+        public class WebControls
+        {
+            public class Label
+            {
+            }
+
+            public class DropDownList
+            {
+            }
+
+            public class TextBox
+            {
+            }
+
+            public class RadioButton
+            {
+            }
+
+            public class FileUpload
+            {
+            }
+        }
+    }
+}
