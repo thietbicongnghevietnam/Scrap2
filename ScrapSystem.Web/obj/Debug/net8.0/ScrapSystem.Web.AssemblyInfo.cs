078b52c51b8677f966c772a0351a9f8e4dc0c2e6
Minh <thietbicongnghevietnam@gmail.com>
2025-12-25 17:24:44 +0700

    25122025b

--- ScrapSystem.Web/obj/Debug/net8.0/ScrapSystem.Web.AssemblyInfo.cs
+++ ScrapSystem.Web/obj/Debug/net8.0/ScrapSystem.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5d7f879ed83cd34a69157f18e4c9f1e9f36c88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6cfdb69171c3437533ee20a61376418f455232")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
